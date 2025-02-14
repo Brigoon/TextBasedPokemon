c545a6320533819620320c8d4dc5be00dd35223f
Ty Dickinson <142946378+tdickinson29@users.noreply.github.com>
2025-02-14 11:16:35 -0600

    Types (#2)
    * custom types
    
    * switch from strings to enum class
    
    * extra getter for ease
    
    * SEEL
    
    * add psychic

---
+++ types.cpp
@@ -0,0 +1,83 @@
+#include <set>
+#include "types.h"
+
+TypeString TSFromString(std::string str)
+{
+    if (str == "normal") { return TypeString::normal; }
+    if (str == "grass") { return TypeString::grass; }
+    if (str == "fire") { return TypeString::fire; }
+    if (str == "water") { return TypeString::water; }
+    if (str == "electric") { return TypeString::electric; }
+    if (str == "ice") { return TypeString::ice; }
+    if (str == "fighting") { return TypeString::fighting; }
+    if (str == "poison") { return TypeString::poison; }
+    if (str == "ground") { return TypeString::ground; }
+    if (str == "flying") { return TypeString::flying; }
+    if (str == "psychic") { return TypeString::psychic; }
+    if (str == "bug") { return TypeString::bug; }
+    if (str == "rock") { return TypeString::rock; }
+    if (str == "ghost") { return TypeString::ghost; }
+    if (str == "dragon") { return TypeString::dragon; }
+    if (str == "dark") { return TypeString::dark; }
+    if (str == "steel") { return TypeString::steel; }
+    if (str == "fairy") { return TypeString::fairy; }
+    throw std::invalid_argument("invalid type " + str);
+}
+
+// for completeness, function to go the other way
+// likely only for visibility purposes to user, should these be capitalized?
+std::string StringFromTS(TypeString type)
+{
+    switch (type)
+    {
+        case TypeString::normal: return "normal";
+        case TypeString::grass: return "grass";
+        case TypeString::fire: return "fire";
+        case TypeString::water: return "water";
+        case TypeString::electric: return "electric";
+        case TypeString::ice: return "ice";
+        case TypeString::fighting: return "fighting";
+        case TypeString::poison: return "poison";
+        case TypeString::ground: return "ground";
+        case TypeString::flying: return "flying";
+        case TypeString::psychic: return "psychic";
+        case TypeString::bug: return "bug";
+        case TypeString::rock: return "rock";
+        case TypeString::ghost: return "ghost";
+        case TypeString::dragon: return "dragon";
+        case TypeString::dark: return "dark";
+        case TypeString::steel: return "steel";
+        case TypeString::fairy: return "fairy";
+    }
+}
+
+DamageType::DamageType(TypeString name_in, std::set<TypeString> strongAgainst_in, std::set<TypeString> weakAgainst_in, std::set<TypeString> immuneTo_in) : name(name_in), strongAgainst(strongAgainst_in), weakAgainst(weakAgainst_in), immuneTo(immuneTo_in)
+{
+}
+
+float CalcTypeEffectiveness(DamageType *defender, DamageType *incoming)
+{
+    float factor;
+    TypeString attackType = incoming->GetTypeName();
+    std::set<TypeString> defendingWeaknesses = defender->GetSuperEffectives();
+    std::set<TypeString> defendingStrengths = defender->GetNotEffectives();
+    std::set<TypeString> defendingImmunes = defender->GetImmunes();
+
+    if (defendingWeaknesses.find(attackType) != defendingWeaknesses.end())
+    {
+        factor = 2;
+    }
+    else if (defendingStrengths.find(attackType) != defendingStrengths.end())
+    {
+        factor = 1.0F / 2;
+    }
+    else if (defendingImmunes.find(attackType) != defendingImmunes.end())
+    {
+        factor = 0;
+    }
+    else
+    {
+        factor = 1;
+    }
+    return factor;
+}
---
+++ types.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <string>
+#include <set>
+
+enum class TypeString
+{
+    normal,
+    grass,
+    fire,
+    water,
+    electric,
+    ice,
+    fighting,
+    poison,
+    ground,
+    flying,
+    psychic,
+    bug,
+    rock,
+    ghost,
+    dragon,
+    dark,
+    steel,
+    fairy
+};
+
+TypeString TSFromString(std::string);
+std::string StringFromTS(TypeString);
+
+class DamageType
+{
+public:
+    DamageType() {}
+    DamageType(TypeString name_in, std::set<TypeString> strongAgainst_in, std::set<TypeString> weakAgainst_in, std::set<TypeString> immuneTo_in = {});
+    ~DamageType() {}
+
+    TypeString GetTypeName() { return name; }
+    string GetStringName() { return StringFromTS(name); }
+    std::set<TypeString> GetNotEffectives() { return strongAgainst; }
+    std::set<TypeString> GetSuperEffectives() { return weakAgainst; }
+    std::set<TypeString> GetImmunes() { return immuneTo; }
+
+private:
+    TypeString name;
+    set<TypeString> strongAgainst;
+    set<TypeString> weakAgainst;
+    set<TypeString> immuneTo;
+};
+float CalcTypeEffectiveness(DamageType *, DamageType *);
+
+class Normal : public DamageType
+{
+public:
+    Normal() : DamageType(TypeString::normal, {}, {TypeString::fighting}, {TypeString::ghost}) {};
+};
+
+class Grass : public DamageType
+{
+public:
+    Grass() : DamageType(TypeString::grass, {TypeString::water, TypeString::electric, TypeString::grass, TypeString::ground}, {TypeString::fire, TypeString::ice, TypeString::poison, TypeString::flying, TypeString::bug}) {};
+};
+
+class Fire : public DamageType
+{
+public:
+    Fire() : DamageType(TypeString::fire, {TypeString::fire, TypeString::grass, TypeString::ice, TypeString::bug, TypeString::steel, TypeString::fairy}, {TypeString::water, TypeString::ground, TypeString::rock}) {};
+};
+
+class Water : public DamageType
+{
+public:
+    Water() : DamageType(TypeString::water, {TypeString::fire, TypeString::water, TypeString::ice, TypeString::steel}, {TypeString::electric, TypeString::grass}) {};
+};
+
+class Ghost : public DamageType
+{
+public:
+    Ghost() : DamageType(TypeString::ghost, {TypeString::ghost, TypeString::dark}, {TypeString::poison, TypeString::bug}, {TypeString::normal, TypeString::fighting}) {};
+};
