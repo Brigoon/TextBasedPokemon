a7b16d26c5bb2826a5d200cf13ac40bea9b1d235
Ty Dickinson <142946378+tdickinson29@users.noreply.github.com>
2026-02-09 12:07:06 -0600

    namespace types (#13)

--- lib/moves.h
+++ lib/moves.h
@@ -28,7 +28,7 @@ class Move
 {
 public:
     Move() {}
-    Move(std::string name_in, DamageType damage_type_in, Categories category_in, int damage_in, 
+    Move(std::string name_in, Types::DamageType damage_type_in, Categories category_in, int damage_in, 
         int accuracy_in, int power_pts_in, int flinch_prob_in, Statuses status_in = Statuses::normal) : 
         name(name_in), damageType(damage_type_in), category(category_in), damage(damage_in), accuracy(accuracy_in), powerPts(power_pts_in), flinchProbability(flinch_prob_in), status(status_in)
     {
@@ -39,7 +39,7 @@ public:
     ~Move() {}
 
     std::string getName() { return name; }
-    DamageType getDamageType() { return damageType; }
+    Types::DamageType getDamageType() { return damageType; }
     Categories getCategory() { return category; }
     int getDamage() { return damage; }
     int getAccuracy() { return accuracy; }
@@ -52,7 +52,7 @@ public:
 
 private:
     std::string name = "";
-    DamageType damageType = Normal();
+    Types::DamageType damageType = Types::normalType;
     Categories category = Categories::physical;
     int damage = 0;
     int accuracy = 0;
--- lib/types.h
+++ lib/types.h
@@ -3,159 +3,180 @@
 #include <string>
 #include <set>
 
-enum class TypeString
-{
-    normal,
-    fire,
-    water,
-    electric,
-    grass,
-    ice,
-    fighting,
-    poison,
-    ground,
-    flying,
-    psychic,
-    bug,
-    rock,
-    ghost,
-    dragon,
-    dark,
-    steel,
-    fairy
-};
+namespace Types {
+    enum class TypeString
+    {
+        normal,
+        fire,
+        water,
+        electric,
+        grass,
+        ice,
+        fighting,
+        poison,
+        ground,
+        flying,
+        psychic,
+        bug,
+        rock,
+        ghost,
+        dragon,
+        dark,
+        steel,
+        fairy
+    };
 
-TypeString TSFromString(std::string);
-std::string StringFromTS(TypeString);
+    TypeString TSFromString(std::string);
+    std::string StringFromTS(TypeString);
 
-class DamageType
-{
-public:
-    DamageType() {}
-    DamageType(TypeString name_in, std::set<TypeString> strongAgainst_in, std::set<TypeString> weakAgainst_in, std::set<TypeString> immuneTo_in = {});
-    ~DamageType() {}
+    class DamageType
+    {
+    public:
+        DamageType() {}
+        DamageType(TypeString name_in, std::set<TypeString> strongAgainst_in, std::set<TypeString> weakAgainst_in, std::set<TypeString> immuneTo_in = {});
+        ~DamageType() {}
 
-    TypeString getTypeName() { return name; }
-    std::string getStringName() { return StringFromTS(name); }
-    std::set<TypeString> getNotEffectives() { return strongAgainst; }
-    std::set<TypeString> getSuperEffectives() { return weakAgainst; }
-    std::set<TypeString> getImmunes() { return immuneTo; }
+        TypeString getTypeName() { return name; }
+        std::string getStringName() { return StringFromTS(name); }
+        std::set<TypeString> getNotEffectives() { return strongAgainst; }
+        std::set<TypeString> getSuperEffectives() { return weakAgainst; }
+        std::set<TypeString> getImmunes() { return immuneTo; }
 
-private:
-    TypeString name;
-    std::set<TypeString> strongAgainst;
-    std::set<TypeString> weakAgainst;
-    std::set<TypeString> immuneTo;
-};
-float calcTypeEffectiveness(DamageType *, DamageType *);
+    private:
+        TypeString name;
+        std::set<TypeString> strongAgainst;
+        std::set<TypeString> weakAgainst;
+        std::set<TypeString> immuneTo;
+    };
+    float calcTypeEffectiveness(DamageType *, DamageType *);
 
 
 
-// subclasses
-class Normal : public DamageType
-{
-public:
-    Normal() : DamageType(TypeString::normal, {}, {TypeString::fighting}, {TypeString::ghost}) {};
-};
+    // subclasses
+    class Normal : public DamageType
+    {
+    public:
+        Normal() : DamageType(TypeString::normal, {}, {TypeString::fighting}, {TypeString::ghost}) {};
+    };
 
-class Fire : public DamageType
-{
-public:
-    Fire() : DamageType(TypeString::fire, {TypeString::fire, TypeString::grass, TypeString::ice, TypeString::bug, TypeString::steel, TypeString::fairy}, {TypeString::water, TypeString::ground, TypeString::rock}) {};
-};
+    class Fire : public DamageType
+    {
+    public:
+        Fire() : DamageType(TypeString::fire, {TypeString::fire, TypeString::grass, TypeString::ice, TypeString::bug, TypeString::steel, TypeString::fairy}, {TypeString::water, TypeString::ground, TypeString::rock}) {};
+    };
 
-class Water : public DamageType
-{
-public:
-    Water() : DamageType(TypeString::water, {TypeString::fire, TypeString::water, TypeString::ice, TypeString::steel}, {TypeString::electric, TypeString::grass}) {};
-};
+    class Water : public DamageType
+    {
+    public:
+        Water() : DamageType(TypeString::water, {TypeString::fire, TypeString::water, TypeString::ice, TypeString::steel}, {TypeString::electric, TypeString::grass}) {};
+    };
 
-class Electric : public DamageType
-{
-public:
-    Electric() : DamageType(TypeString::electric, {TypeString::electric, TypeString::flying, TypeString::steel}, {TypeString::ground}) {};
-};
+    class Electric : public DamageType
+    {
+    public:
+        Electric() : DamageType(TypeString::electric, {TypeString::electric, TypeString::flying, TypeString::steel}, {TypeString::ground}) {};
+    };
 
-class Grass : public DamageType
-{
-public:
-    Grass() : DamageType(TypeString::grass, {TypeString::water, TypeString::electric, TypeString::grass, TypeString::ground}, {TypeString::fire, TypeString::ice, TypeString::poison, TypeString::flying, TypeString::bug}) {};
-};
+    class Grass : public DamageType
+    {
+    public:
+        Grass() : DamageType(TypeString::grass, {TypeString::water, TypeString::electric, TypeString::grass, TypeString::ground}, {TypeString::fire, TypeString::ice, TypeString::poison, TypeString::flying, TypeString::bug}) {};
+    };
 
-class Ice : public DamageType
-{
-public:
-    Ice() : DamageType(TypeString::ice, {TypeString::ice}, {TypeString::fire, TypeString::fighting, TypeString::rock, TypeString::steel}) {};
-};
+    class Ice : public DamageType
+    {
+    public:
+        Ice() : DamageType(TypeString::ice, {TypeString::ice}, {TypeString::fire, TypeString::fighting, TypeString::rock, TypeString::steel}) {};
+    };
 
-class Fighting : public DamageType
-{
-public:
-    Fighting() : DamageType(TypeString::fighting, {TypeString::bug, TypeString::rock, TypeString::dark}, {TypeString::flying, TypeString::psychic, TypeString::fairy}) {};
-};
+    class Fighting : public DamageType
+    {
+    public:
+        Fighting() : DamageType(TypeString::fighting, {TypeString::bug, TypeString::rock, TypeString::dark}, {TypeString::flying, TypeString::psychic, TypeString::fairy}) {};
+    };
 
-class Poison : public DamageType
-{
-public:
-    Poison() : DamageType(TypeString::poison, {TypeString::grass, TypeString::fighting, TypeString::poison, TypeString::bug, TypeString::fairy}, {TypeString::ground, TypeString::psychic}) {};
-};
+    class Poison : public DamageType
+    {
+    public:
+        Poison() : DamageType(TypeString::poison, {TypeString::grass, TypeString::fighting, TypeString::poison, TypeString::bug, TypeString::fairy}, {TypeString::ground, TypeString::psychic}) {};
+    };
 
-class Ground : public DamageType
-{
-public:
-    Ground() : DamageType(TypeString::ground, {TypeString::poison, TypeString::rock}, {TypeString::water, TypeString::grass, TypeString::ice}, {TypeString::electric}) {};
-};
+    class Ground : public DamageType
+    {
+    public:
+        Ground() : DamageType(TypeString::ground, {TypeString::poison, TypeString::rock}, {TypeString::water, TypeString::grass, TypeString::ice}, {TypeString::electric}) {};
+    };
 
-class Flying : public DamageType
-{
-public:
-    Flying() : DamageType(TypeString::flying, {TypeString::grass, TypeString::fighting, TypeString::bug}, {TypeString::electric, TypeString::ice, TypeString::rock}, {TypeString::ground}) {};
-};
+    class Flying : public DamageType
+    {
+    public:
+        Flying() : DamageType(TypeString::flying, {TypeString::grass, TypeString::fighting, TypeString::bug}, {TypeString::electric, TypeString::ice, TypeString::rock}, {TypeString::ground}) {};
+    };
 
-class Psychic : public DamageType
-{
-public:
-    Psychic() : DamageType(TypeString::psychic, {TypeString::fighting, TypeString::psychic}, {TypeString::bug, TypeString::ghost, TypeString::dark}) {};
-};
+    class Psychic : public DamageType
+    {
+    public:
+        Psychic() : DamageType(TypeString::psychic, {TypeString::fighting, TypeString::psychic}, {TypeString::bug, TypeString::ghost, TypeString::dark}) {};
+    };
 
-class Bug : public DamageType
-{
-public:
-    Bug() : DamageType(TypeString::bug, {TypeString::grass, TypeString::fighting, TypeString::ground}, {TypeString::fire, TypeString::flying, TypeString::rock}) {};
-};
+    class Bug : public DamageType
+    {
+    public:
+        Bug() : DamageType(TypeString::bug, {TypeString::grass, TypeString::fighting, TypeString::ground}, {TypeString::fire, TypeString::flying, TypeString::rock}) {};
+    };
 
-class Rock : public DamageType
-{
-public:
-    Rock() : DamageType(TypeString::rock, {TypeString::normal, TypeString::fire, TypeString::poison, TypeString::flying}, {TypeString::water, TypeString::grass, TypeString::fighting, TypeString::ground, TypeString::steel}) {};
-};
+    class Rock : public DamageType
+    {
+    public:
+        Rock() : DamageType(TypeString::rock, {TypeString::normal, TypeString::fire, TypeString::poison, TypeString::flying}, {TypeString::water, TypeString::grass, TypeString::fighting, TypeString::ground, TypeString::steel}) {};
+    };
 
-class Ghost : public DamageType
-{
-public:
-    Ghost() : DamageType(TypeString::ghost, {TypeString::poison, TypeString::bug}, {TypeString::ghost, TypeString::dark}, {TypeString::normal, TypeString::fighting}) {};
-};
+    class Ghost : public DamageType
+    {
+    public:
+        Ghost() : DamageType(TypeString::ghost, {TypeString::poison, TypeString::bug}, {TypeString::ghost, TypeString::dark}, {TypeString::normal, TypeString::fighting}) {};
+    };
 
-class Dragon : public DamageType
-{
-public:
-    Dragon() : DamageType(TypeString::dragon, {TypeString::fire, TypeString::water, TypeString::electric, TypeString::grass}, {TypeString::ice, TypeString::dragon, TypeString::fairy}) {};
-};
+    class Dragon : public DamageType
+    {
+    public:
+        Dragon() : DamageType(TypeString::dragon, {TypeString::fire, TypeString::water, TypeString::electric, TypeString::grass}, {TypeString::ice, TypeString::dragon, TypeString::fairy}) {};
+    };
 
-class Dark : public DamageType
-{
-public:
-    Dark() : DamageType(TypeString::dark, {TypeString::ghost, TypeString::dark}, {TypeString::fighting, TypeString::bug, TypeString::fairy}, {TypeString::psychic}) {};
-};
+    class Dark : public DamageType
+    {
+    public:
+        Dark() : DamageType(TypeString::dark, {TypeString::ghost, TypeString::dark}, {TypeString::fighting, TypeString::bug, TypeString::fairy}, {TypeString::psychic}) {};
+    };
 
-class Steel : public DamageType
-{
-public:
-    Steel() : DamageType(TypeString::steel, {TypeString::normal, TypeString::grass, TypeString::ice, TypeString::flying, TypeString::psychic, TypeString::bug, TypeString::rock, TypeString::dragon, TypeString::steel, TypeString::fairy}, {TypeString::fire, TypeString::fighting, TypeString::ground}, {TypeString::poison}) {};
-};
+    class Steel : public DamageType
+    {
+    public:
+        Steel() : DamageType(TypeString::steel, {TypeString::normal, TypeString::grass, TypeString::ice, TypeString::flying, TypeString::psychic, TypeString::bug, TypeString::rock, TypeString::dragon, TypeString::steel, TypeString::fairy}, {TypeString::fire, TypeString::fighting, TypeString::ground}, {TypeString::poison}) {};
+    };
 
-class Fairy : public DamageType
-{
-public:
-    Fairy() : DamageType(TypeString::fairy, {TypeString::fighting, TypeString::bug, TypeString::dark}, {TypeString::bug, TypeString::steel}, {TypeString::dragon}) {};
-};
+    class Fairy : public DamageType
+    {
+    public:
+        Fairy() : DamageType(TypeString::fairy, {TypeString::fighting, TypeString::bug, TypeString::dark}, {TypeString::bug, TypeString::steel}, {TypeString::dragon}) {};
+    };
+
+    extern Normal normalType;
+    extern Fire fireType;
+    extern Water waterType;
+    extern Electric electricType;
+    extern Grass grassType;
+    extern Ice iceType;
+    extern Fighting fightingType;
+    extern Poison poisonType;
+    extern Ground groundType;
+    extern Flying flyingType;
+    extern Psychic psychicType;
+    extern Bug bugType;
+    extern Rock rockType;
+    extern Ghost ghostType;
+    extern Dragon dragonType;
+    extern Dark darkType;
+    extern Steel steelType;
+    extern Fairy fairyType;
+}
